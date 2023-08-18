bf2fdba9d86bf45136a42bd5a7295cfa3fb35924
shyonae <scionae@gmail.com>
2023-08-18 14:39:35 +0200

    modified setuo

--- initial_setup.sh
+++ initial_setup.sh
@@ -25,10 +25,14 @@ echo "Setting permissions on folders..."
 sudo chown -R $(whoami):mediaServer /media # sudo chown -R shyonae:mediaServer /media
 sudo chmod -R a=,a+rX,u+w,g+w /media       # sudo chmod -R a=,a+rX,u+w,g+w /media
 
+### JQ INSTALL ###
+echo "Installing JQ..."
+sudo apt-get update
+sudo apt-get install -y jq
+
 ### DOCKER INSTALL ###
 echo "Installing Docker and dependencies..."
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -37,10 +41,15 @@ echo \
     "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
     sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "Creating bridge network shyDockerServer..."
 docker network create -d bridge shyDockerServer
+
+### GRAFANA LOKI DOCKER PLUGIN INSTALL ###
+docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
+sudo touch /etc/docker/daemon.json
+sudo runuser -l root -c 'jq -n "{debug: true, \"log-driver\": \"loki\", \"log-opts\": { \"loki-url\": \"http://localhost:3100/loki/api/v1/push\", \"loki-batch-size\": \"400\"}}" > /etc/docker/daemon.json'
 
 ### INFISICAL INSTALL ###
 echo "Installing Infisical..."
@@ -54,19 +63,19 @@ infisical login -i
 echo "Installing Sublime Text..."
 sudo wget -O- https://download.sublimetext.com/sublimehq-pub.gpg | gpg -dearmor | sudo tee /usr/share/keyrings/sublimehq.gpg
 echo 'deb [signed-by=/usr/share/keyrings/sublimehq.gpg] https://download.sublimetext.com/ apt/stable/' | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update && sudo apt install sublime-text
+sudo apt update && sudo apt install -y sublime-text
 
 ### TELEGRAM INSTALL ###
 echo "Installing Telegram..."
-sudo apt install telegram-desktop
+sudo apt install -y telegram-desktop
 
 ### BRAVE INSTALL ###
 echo "Installing Brave..."
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update && sudo apt install brave-browser
+sudo apt update && sudo apt install -y brave-browser
 
 ### TABBY INSTALL###
 echo "Installing Tabby..."
 sudo curl -s https://packagecloud.io/install/repositories/eugeny/tabby/script.deb.sh | sudo bash
-sudo apt install tabby-terminal
+sudo apt install -y tabby-terminal
