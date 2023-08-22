b16cb38571cdd2b59969fe47c23120de64f63d39
shyonae <88051140+shyonae@users.noreply.github.com>
2023-08-22 13:27:57 +0000

    moved everything to new folders

---
+++ scripts/down_all.sh
@@ -0,0 +1,3 @@
+for compose in $(find $PWD -type f -not -path '*.unused*' -name 'docker-compose.yml'); do
+  infisical run -- docker compose -f $compose down
+done
--- start.sh
+++
@@ -1,5 +0,0 @@
-compose_files=("portainer/docker-compose.yml" "dockerproxy/docker-compose.yml" "authelia/docker-compose.yml" "bazarr/docker-compose.yml" "bazarranime/docker-compose.yml" "cloudflare-ddns/docker-compose.yml" "code-server/docker-compose.yml" "crowdsec/docker-compose.yml" "flaresolverr/docker-compose.yml" "homarr/docker-compose.yml" "jellyfin/docker-compose.yml" "jellyseerr/docker-compose.yml" "notifiarr/docker-compose.yml" "prowlarr/docker-compose.yml" "qbittorrent/docker-compose.yml" "radarr/docker-compose.yml" "radarranime/docker-compose.yml" "sonarr/docker-compose.yml" "sonarranime/docker-compose.yml" "swag/docker-compose.yml" "wizarr/docker-compose.yml" "yacht/docker-compose.yml" "guacamole/docker-compose.yml" "wireguard/docker-compose.yml" "watchtower/docker-compose.yml" "prometheus/docker-compose.yml" "firefly_iii/docker-compose.yml")
-
-for compose in ${compose_files[@]}; do
-  infisical run -- docker compose -f $compose up -d
-done
